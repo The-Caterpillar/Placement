043d854bc46dd9c14170f724c80a326fa00df7be
Saraswati <115623683+The-Caterpillar@users.noreply.github.com>
2023-01-26 00:09:26 +0530

    Update sqrt(x).cpp

--- Dsa/Binary Search/sqrt(x).cpp
+++ Dsa/Binary Search/sqrt(x).cpp
@@ -1,1 +1,18 @@
-bdvshegf
+class Solution {
+public:
+    int mySqrt(int x) 
+    {
+        long long mid;
+        int low=1, high = 100000;
+
+        while(low<=high)
+        {
+            mid = low + (high-low)/2;
+            if(mid*mid==x) return mid;
+            else if(mid*mid>x) high = mid-1;
+            else if(mid*mid<x) low = mid+1;
+        }
+        if(mid*mid<=x) return mid;
+    return mid-1;        
+    }
+};
